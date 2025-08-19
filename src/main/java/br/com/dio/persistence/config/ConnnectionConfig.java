fe006b437fd3f20356fda28d495dbeeefa84ad2b
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-19 12:03:40 -0300

    Refactor: removendo algumuns comentários

--- src/main/java/br/com/dio/persistence/config/ConnnectionConfig.java
+++ src/main/java/br/com/dio/persistence/config/ConnnectionConfig.java
@@ -4,17 +4,18 @@ import lombok.NoArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 import static lombok.AccessLevel.PRIVATE;
 
 @NoArgsConstructor(access = PRIVATE)
 public final class ConnnectionConfig {
-    public static <SQLException> Connection getConnection() throws java.sql.SQLException {
+    public static Connection getConnection() throws java.sql.SQLException {
         String url = "jdbc:postgresql://localhost:5432/meubanco";
         String user = "postgres";
         String password = "minhasenha";
+        //Estabelecendo uma conexão com o banco de dados
         var connection = DriverManager.getConnection(url, user, password);
+        //para não salvar automaticamente no vanco de dados
         connection.setAutoCommit(false);
         return connection;
     }
